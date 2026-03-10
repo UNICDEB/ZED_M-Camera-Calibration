cfce66b33cf5546152da56857e92b4e35e2d7fae
UNICDEB <doloidebabrat99@gmail.com>
2026-03-10 16:10:38 +0530

    Initial Update(version_1.0.0.3)

--- testing.py
+++ testing.py
@@ -518,7 +518,7 @@ def main():
     zed = sl.Camera()
 
     init_params = sl.InitParameters()
-    init_params.camera_resolution = sl.RESOLUTION.HD1080
+    init_params.camera_resolution = sl.RESOLUTION.HD720
     init_params.camera_fps = 30
     init_params.depth_mode = sl.DEPTH_MODE.ULTRA
     init_params.coordinate_units = sl.UNIT.METER
